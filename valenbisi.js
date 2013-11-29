e8fdcfdfaf292b939da0efe5e907ac1025d57367
Javier <spamalo@gmail.com>
2013-11-29 15:09:28 +0100

    Update valenbisi.js
    added new function. And insert messages inside a div

--- valenbisi.js
+++ valenbisi.js
@@ -1,15 +1,22 @@
+function showBikes (name, station){
 	  var xhr = new XMLHttpRequest();
-       xhr.open("GET", "http://www.valenbisi.es/service/stationdetails/valence/14", true);
+       xhr.open("GET", "http://www.valenbisi.es/service/stationdetails/valence/" + station, true);
 	   xhr.onreadystatechange = function() {
           if (xhr.readyState == 4) {
             if (xhr.status == 200) {
-              //alert(xhr.responseText);
-			  //document.write(xhr.responseText);
 			  var free = xhr.responseXML.getElementsByTagName("free")[0];
 			  var bicis = xhr.responseXML.getElementsByTagName("available")[0];
-			  document.writeln("bicis: " + bicis.textContent);
-              document.writeln("puestos: " + free.textContent);
+			  var report= document.getElementById("report");
+  			  var newBike = "<div><strong>" + name + "</strong>" 
+						  + " bicis: " +  bicis.textContent
+						  +	" puestos: " + free.textContent
+						  + "</div> <br/>";
+			  report.innerHTML = report.innerHTML + newBike;
             }
           }
         };
       xhr.send();
+}
+
+showBikes("test1", 1);
+showBikes("test2", 2);
